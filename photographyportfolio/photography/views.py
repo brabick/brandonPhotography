df418dbf75bcdd17841be1bbc139ac3f10b877ff
Brandon <brabick@gmail.com>
2022-12-19 13:48:55 -0700

    Initial commit. Most of the bones have been built out with a couple of simple views and templates.

---
+++ photographyportfolio/photography/views.py
@@ -0,0 +1,29 @@
+import os
+from os.path import join
+
+from django.http import HttpResponse
+from django.shortcuts import render
+
+
+def getImages(dir):
+    images = {'data': []}
+    directory = "C:\PhotographyPortfolio\photographyportfolio\photography\static" + dir
+    for file in os.listdir(directory):
+        # f = os.path.join(directory, file)
+        f = dir + file
+        images['data'].append('../../static' + f)
+
+    print(images)
+    return images
+
+
+def index(request):
+    images = getImages("/img/")
+
+    return render(request, 'pages/index.html', images)
+
+def landscape(request):
+    images = getImages("/landscape/")
+
+    return render(request, 'pages/landscape.html', images)
+
