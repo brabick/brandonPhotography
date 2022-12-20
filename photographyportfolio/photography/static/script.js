a2dd30b64ddeeebd2bc7a7a450ca783c7482bc80
Brandon <brabick@gmail.com>
2022-12-20 10:03:07 -0700

    Mostly have the views where I want them. Added Post model for my blog stuff (if that sticks)

--- photographyportfolio/photography/static/script.js
+++ photographyportfolio/photography/static/script.js
@@ -1,4 +1,14 @@
 function scrollerington() {
-    document.getElementById("carouselIndicators").scrollBy(event.clientX,0);
-    //el.documentElement.scrollBy(x,y);
+    let element = document.getElementById("carouselIndicators")
+    let maxScrollLeft = element.scrollWidth - element.clientWidth;
+    //alert(maxScrollLeft);
+    let left = event.clientX - element.offsetLeft;
+    //alert(left);
+    if (maxScrollLeft <= left) {
+        //alert(element.scrollLeft);
+        element.scrollLeft += (left / 3);
+    } else {
+        //alert(element.scrollLeft);
+        element.scrollLeft -= (left);
+    }
 }
--- photographyportfolio/photography/views.py
+++ photographyportfolio/photography/views.py
@@ -3,6 +3,9 @@ from os.path import join
 
 from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import generic
+
+from photographyportfolio.photography.models import Post
 
 
 def getImages(dir):
@@ -22,12 +25,26 @@ def index(request):
 
     return render(request, 'pages/index.html', images)
 
+
 def landscape(request):
     images = getImages("/landscape/")
 
     return render(request, 'pages/landscape.html', images)
 
+
 def contact(request):
 
     return render(request, 'pages/contact.html')
 
+
+def post_list(request):
+    queryset = Post.objects.
+    template_name = 'index.html'
+
+    return render(request, 'pages/postlist.html')
+
+
+def post_detail(request):
+    post = Post.objects.filter
+
+    return render(request, 'pages/postdetail.html')
